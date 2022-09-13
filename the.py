be0397f118f6afb989d665feef912a0184c0421a
Evanzap <49214424+Evanzap@users.noreply.github.com>
2022-09-13 10:20:04 -0600

    fixed about command to show real.

--- the.py
+++ the.py
@@ -103,7 +103,7 @@ async def givehelp(ctx, member : discord.Member):
 
 @bot.command(help="Displays information about the bot.", )
 async def about(ctx):
-    embed=discord.Embed(title="Joe Bot Testing",url="https://github.com/joshuavanderbilt/joebot-testing",description="This is a JOE bot, all hail Joe! Contributers: JoshuaMV.",color=0xFF5733)
+    embed=discord.Embed(title="Joe Bot v6.4.5",url="https://github.com/Evanzap/joebot",description="This is a JOE bot, all hail Joe! Contributers: JoshuaMV.",color=0xFF5733)
     embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/1015749450544730204/5cd7886d908ad187abec6d3defc3c5bb.webp")
     await ctx.send(embed=embed)
     print("User called the about message.")
